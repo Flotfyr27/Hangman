354ee2bd000169c640bc960e7fb25bfc8b092fdc
LGramme <42938246+LGramme@users.noreply.github.com>
2019-10-20 13:58:21 +0200

    Removed keyboard temporarily, added methods in mainActivity to get screensize.

--- app/src/main/java/dk/gramme/dtu/hangman/MainActivity.java
+++ app/src/main/java/dk/gramme/dtu/hangman/MainActivity.java
@@ -2,14 +2,26 @@ package dk.gramme.dtu.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public static int getScreenWidth(){
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight(){
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 }
