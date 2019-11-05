b2b48ecbf4e3f8e2142c31a29ef4adbc992b96e9
LGramme <42938246+LGramme@users.noreply.github.com>
2019-11-05 22:56:55 +0100

    saved inflated layout in variable

--- app/src/main/java/dk/gramme/dtu/hangman/HighscoreFragment.java
+++ app/src/main/java/dk/gramme/dtu/hangman/HighscoreFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ public class HighscoreFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_highscore, container, false);
+        View v = inflater.inflate(R.layout.fragment_highscore, container, false);
+        return v;
     }
 }
